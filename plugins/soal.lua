d831fe0be515addf208df02f264d5de9e155ce3d
seyed1955 <dj.seyed1955.99@gmail.com>
2016-09-26 17:07:04 +0330

    Update and rename solid.lua to soal.lua

--- plugins/soal.lua
+++ plugins/soal.lua
@@ -2,9 +2,9 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'بلی؟'
+local text = 'بله ؟ بفرمایید ،من ابوالفضلم'
 --در اینجا میتونین جواب رو تعیین کنید
-if matches[1] == 'سلید' or 'solid' or 'سعید' or 'saeid' or 'saeed' then
+if matches[1] == 'ابول' or 'abolfazl' or 'ابوالفضل' or 'abol' then
     if not is_sudo(msg) then
 --در این قسمت میتونید اسم خودتون رو بزارید
 --یادتون باشه وقتی اسم خودتون رو گذاشتید پترن هارو هم تغییر بدید
@@ -14,11 +14,10 @@ end
 end
 return {
 patterns = {
-    "^سلید$",
-    "^سعید$",
-"^([Ss]aeid)$",
-"^([Ss]aeed)$",
-"^([Ss]olid)$"
+    "^ابوالفضل$",
+    "^ابول$",
+"^([Aa]bolfazl)$",
+"^([Aa]bol)$",
 },
 run = run
 }
